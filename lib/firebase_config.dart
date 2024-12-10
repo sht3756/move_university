64b7277b513998b67c89ba3a94ef5f3db30313da
SHT-3756 <gkdlfndkekd@naver.com>
2024-12-10 15:06:40 +0900

    feat : firebase config 추가

---
+++ lib/firebase_config.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class FirebaseConfig {
+  static FirebaseOptions get options {
+    return FirebaseOptions(
+      apiKey:  dotenv.env[Platform.isAndroid ? 'ANDROID_API_KEY' : 'IOS_APP_ID']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+      storageBucket: dotenv.env['STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      appId: dotenv.env[Platform.isAndroid ? 'ANDROID_APP_ID': 'IOS_APP_ID']!,
+    );
+  }
+}
