71b3f7e2374b9bbe9610ecb2d3e01b000528641e
SHT-3756 <gkdlfndkekd@naver.com>
2024-12-11 22:52:01 +0900

    fix : 이름 오타 수정

--- lib/firebase_config.dart
+++ lib/firebase_config.dart
@@ -5,7 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class FirebaseConfig {
   static FirebaseOptions get options {
     return FirebaseOptions(
-      apiKey:  dotenv.env[Platform.isAndroid ? 'ANDROID_API_KEY' : 'IOS_APP_ID']!,
+      apiKey:  dotenv.env[Platform.isAndroid ? 'ANDROID_API_KEY' : 'IOS_APP_KEY']!,
       projectId: dotenv.env['PROJECT_ID']!,
       storageBucket: dotenv.env['STORAGE_BUCKET']!,
       messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
